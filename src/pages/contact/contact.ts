811763d019386495bbf658376c2bf2dead1e2eed
Guilherme Henrique PC UTFPR <guilhermen@alunos.utfpr.edu.br>
2019-08-22 20:56:33 -0300

    Iniciando o projeto Contacts

---
+++ src/pages/contact/contact.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html'
+})
+export class ContactPage {
+
+  contacts: any;
+  constructor(public navCtrl: NavController) {
+  }
+
+
+}
---
+++ src/pages/create-contact/create-contact.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ContactsProvider } from '../../providers/contacts/contacts';
+
+/**
+ * Generated class for the CreateContactPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-create-contact',
+  templateUrl: 'create-contact.html',
+})
+export class CreateContactPage {
+
+  model: Contact;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.model = new Contact();
+    this.model.name = 'Novo contato';
+    this.model.gender = 'male';
+
+  }
+
+  createContact(){
+    console.log("form");
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CreateContactPage');
+  }
+
+}
+
+export class Contact {
+  name: string;
+  gender: string;
+}
